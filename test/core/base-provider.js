5526d43e0a48106ceadf3ee57ef1781db477f092
rajikaimal <inbox.rajika@gmail.com>
2017-05-18 20:02:45 +0530

    Add base provider

---
+++ test/core/base-provider.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const assert = chai.assert;
+const ncProvider = require('../../lib/core/base-provider');
+const providers = require('../../lib/providers');
+
+describe('Base provider', () => {
+  it('should return AWS provider', (done) => {
+    const ncAWS = new ncProvider(providers.AWS);
+
+    assert.typeOf(ncAWS, 'object');
+    done();
+  });
+});
