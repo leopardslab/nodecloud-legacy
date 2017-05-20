8cf777cb58dbc67ff3f93e9005a3baea82998a4e
rajikaimal <inbox.rajika@gmail.com>
2017-05-20 13:07:55 +0530

    More EC2 examples

--- examples/AWS/EC2.js
+++ examples/AWS/EC2.js
@@ -1,27 +1,85 @@
 const nodeCloud = require("../../lib/");
-const ncAWS = nodeCloud.getProvider("AWS");
+const ncAWS = nodeCloud.getProvider(
+  "AWS",
+  "/PATH/TO/config.json"
+);
 const options = {
   apiVersion: "2016-11-15"
 };
 
-const params = {
-  ImageId: "ami-10fd7020", // amzn-ami-2011.09.1.x86_64-ebs
-  InstanceType: "t1.micro",
-  MinCount: 1,
-  MaxCount: 1
-};
-const instanceParams = {
-  Key: "Name",
-  Value: "Node Cloud demo"
-};
+// const params = {
+//   ImageId: "ami-10fd7020", // amzn-ami-2011.09.1.x86_64-ebs
+//   InstanceType: "t1.micro",
+//   MinCount: 1,
+//   MaxCount: 1
+// };
+// const instanceParams = {
+//   Key: "Name",
+//   Value: "Node Cloud demo"
+// };
 
 const ec2 = ncAWS.EC2(options);
 
-ec2
-  .createInstance(params, instanceParams)
-  .then(res => {
-    console.log(`All done ! ${res}`);
-  })
-  .catch(err => {
-    console.log(`Oops something happened ${err}`);
-  });
+// ec2
+//   .create(params, instanceParams)
+//   .then(res => {
+//     console.log(`All done ! ${res}`);
+//   })
+//   .catch(err => {
+//     console.log(`Oops something happened ${err}`);
+//   });
+
+// const params = {
+//   DryRun: false
+// };
+
+// ec2.
+//   list(params)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+const params = {
+  InstanceIds: [""],
+  DryRun: false
+};
+
+// ec2
+//   .stop(params)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// ec2
+//   .start(params)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// ec2
+//   .reboot(params)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// ec2
+//   .destroy(params)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
