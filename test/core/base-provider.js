0b7fd935eb59c9bcfc052e0228e2ebf8c864f0a7
rajikaimal <inbox.rajika@gmail.com>
2017-05-21 01:33:39 +0530

    Add tests

--- test/core/base-provider.js
+++ test/core/base-provider.js
@@ -1,13 +1,16 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
-const ncProvider = require('../../lib/core/base-provider');
-const providers = require('../../lib/providers');
+const ncProvider = require("../../lib/core/base-provider");
+const providers = require("../../lib/providers");
 
-describe('Base provider', () => {
-  it('should return AWS provider', (done) => {
-    const ncAWS = new ncProvider(providers.AWS);
+describe("Base provider", () => {
+  it("should return AWS provider", done => {
+    const ncAWS = new ncProvider(
+      providers.AWS,
+      "/home/rajika/projects/gsoc/config.json"
+    );
 
-    assert.typeOf(ncAWS, 'object');
+    assert.typeOf(ncAWS, "object");
     done();
   });
 });
