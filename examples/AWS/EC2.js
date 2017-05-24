b65427eabc1028c4dc22e41c5e0537973e9cc55f
rajikaimal <inbox.rajika@gmail.com>
2017-05-24 09:34:10 +0530

    Update examples

--- examples/AWS/EC2.js
+++ examples/AWS/EC2.js
@@ -1,85 +1,29 @@
 const nodeCloud = require("../../lib/");
-const ncAWS = nodeCloud.getProvider(
-  "AWS",
-  "/home/rajika/projects/gsoc/examples/AWS/config.json"
-);
+const ncAWS = nodeCloud.getProvider("AWS", process.env.ncconf);
 const options = {
   apiVersion: "2016-11-15"
 };
-
-// const params = {
-//   ImageId: "ami-10fd7020", // amzn-ami-2011.09.1.x86_64-ebs
-//   InstanceType: "t1.micro",
-//   MinCount: 1,
-//   MaxCount: 1
-// };
-// const instanceParams = {
-//   Key: "Name",
-//   Value: "Node Cloud demo"
-// };
-
-const ec2 = ncAWS.EC2(options);
-
-// ec2
-//   .create(params, instanceParams)
-//   .then(res => {
-//     console.log(`All done ! ${res}`);
-//   })
-//   .catch(err => {
-//     console.log(`Oops something happened ${err}`);
-//   });
-
-// const params = {
-//   DryRun: false
-// };
-
-// ec2.
-//   list(params)
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
 const params = {
-  InstanceIds: ["i-0e8463a71076e3d5f"],
-  DryRun: false
+  ImageId: "ami-10fd7020",
+  InstanceType: "t1.micro",
+  MinCount: 1,
+  MaxCount: 1
 };
 
-// ec2
-//   .stop(params)
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+const instanceParams = {
+  Key: "Name",
+  Value: "Node Cloud demo"
+};
 
-// ec2
-//   .start(params)
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+// get compute object for AWS
+const ec2 = ncAWS.compute(options);
 
-// ec2
-//   .reboot(params)
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-// ec2
-//   .destroy(params)
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
+// create AWS EC2 instance
+ec2
+  .create(params, instanceParams)
+  .then(res => {
+    console.log(`All done ! ${res}`);
+  })
+  .catch(err => {
+    console.log(`Oops something happened ${err}`);
+  });
