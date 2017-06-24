21d30399473a8328993a4ac22d974cb380a87e73
rajikaimal <inbox.rajika@gmail.com>
2017-06-24 09:06:00 +0530

    Add AWS S3 example

--- examples/storage/aws-s3.js
+++ examples/storage/aws-s3.js
@@ -1,7 +1,7 @@
-const nodeCloud = require('../../lib/');
-const ncAWS = nodeCloud.getProvider('AWS', process.env.ncconf);
+const nodeCloud = require("../../lib/");
+const ncAWS = nodeCloud.getProvider("AWS", process.env.ncconf);
 const options = {
-  apiVersion: '2016-11-15',
+  apiVersion: "2016-11-15"
 };
 
 // get compute object for AWS
@@ -9,7 +9,7 @@ const s3 = ncAWS.s3(options);
 
 // create S3 Bucket
 const params = {
-  Bucket: 'ncbucketcr',
+  Bucket: 'ncbucketcr1',
   CreateBucketConfiguration: {
     LocationConstraint: 'us-west-2',
   },
@@ -21,7 +21,9 @@ s3
   .create(params)
   .then(res => {
     console.log(`Bucket created ! ${res}`);
+    console.log(res);
   })
   .catch(err => {
     console.error(`Oops something happened ${err}`);
   });
+
